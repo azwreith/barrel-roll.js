aa070e3bed29fbf09b7526710a1b1cc90c7e5ca8
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-27 02:15:13 -0400

    Add main script

---
+++ src/barrelRoll.js
@@ -0,0 +1,37 @@
+var canHazRollin = true;
+
+bRoller = function() {
+
+	var s = document.body.style;
+
+	s.setProperty("-moz-transform", "rotate(360deg)");
+	s.setProperty("-moz-transition-duration", "4s");
+	s.setProperty("-moz-transition-property", "all");
+	s.setProperty("-webkit-transform", "rotate(360deg)");
+	s.setProperty("-webkit-transition-duration", "4s");
+	s.setProperty("-webkit-transition-property", "all");
+	s.setProperty("-o-transform", "rotate(360deg)");
+	s.setProperty("-o-transition-duration", "4s");
+	s.setProperty("-o-transition-property", "all");
+
+	setTimeout(function() {
+		s.removeProperty("-moz-transform");
+		s.removeProperty("-moz-transition-duration");
+		s.removeProperty("-moz-transition-property");
+		s.removeProperty("-webkit-transform");
+		s.removeProperty("-webkit-transition-duration");
+		s.removeProperty("-webkit-transition-property");
+		s.removeProperty("-o-transform");
+		s.removeProperty("-o-transition-duration");
+		s.removeProperty("-o-transition-property");
+		canHazRollin = true;
+	}, 4000);
+
+};
+
+doBarrelRoll = function() {
+	if(canHazRollin) {
+		canHazRollin = false;
+		bRoller();
+	}
+};
