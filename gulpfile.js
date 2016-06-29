1b7fd7398ab029021df9d30a67dcb34683073024
Ujjwal Arora <arora.ujjwal@gmail.com>
2016-06-29 03:26:11 -0400

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify');
+
+gulp.task('minify-js', function() {
+	return gulp.src('/src/barrel-roll.js')
+		.pipe(gulp.dest('/dist'))
+		.pipe(rename('barrel-roll-min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('/dest'));
+});
